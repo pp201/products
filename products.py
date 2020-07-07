d404283454203ab4dbb4cd994f2fac2cae92015a
Baron <popo333555@gmail.com>
2020-07-07 17:59:14 +0200

    refactor

--- products.py
+++ products.py
@@ -1,35 +1,56 @@
-import os
+import os #operating system
+# important concept: refactor
 
 # read file
-products = []
-if os.path.isfile('products.csv'): # check if the file is there
-	print('yeah! found it')
-	with open('products.csv', 'r') as f:
+# 1 function for doing 1 thing
+def read_file(filename):
+	products = []
+	with open(filename, 'r') as f:
 		for line in f:
 			if 'item, price' in line:
 				continue
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
-else:
-	print('><! Cannot find it')
+	return products #with return, you can save the result of excecuting function
 
 # user input
-while True:
-	name = input('Enter item: ')
-	if name == 'q':
-		break
-	price = input('Enter price: ')
-	small_list = [name, price]
-	products.append(small_list)
-print(products)
+def user_input(products):
+	while True:
+		name = input('Enter item: ')
+		if name == 'q':
+			break
+		price = input('Enter price: ')
+		small_list = [name, price]
+		products.append(small_list)
+	print(products)
+	return products
 
 # print out the purchase record
-for p in products:
-	print('The price of', p[0], 'is', p[1])
+def print_products(products):
+	for p in products:
+		print('The price of', p[0], 'is', p[1])
 
 # Write file
-with open ('products.csv', 'w') as f:
-	f.write('item, price\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open (filename, 'w') as f:
+		f.write('item, price\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+#Execute mainly funciotn
+def main(): 
+	filename = 'products.csv'
+	if os.path.isfile(filename): # check if the file is there
+		print('yeah! found it')
+		products = read_file(filename)		
+	else:
+		print('><! Cannot find it')
+		
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+
+main()
+
+
